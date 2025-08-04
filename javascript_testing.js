cd95872ab0cee0e587bf8ed83cc1af18f05cb3a9
Jeff Miller <31397598+jmogl@users.noreply.github.com>
2025-08-03 21:31:50 -0700

    Update javascript_testing.js

--- javascript_testing.js
+++ javascript_testing.js
@@ -1,5 +1,4 @@
 
-ttttt
 // 3D Javacript Clock using three.js
 // Goal is to have a realistic 3D depth with tilt on mobile devices
 // MIT License. - Work in Progress using Gemini
@@ -687,4 +686,5 @@ window.addEventListener('resize', () => {
 
 setupTiltControls();
 animate();
+
 
