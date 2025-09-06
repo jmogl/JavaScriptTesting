4835b75ce8a0d6c480375fd96dcaeb59bf309393
Jeff Miller <31397598+jmogl@users.noreply.github.com>
2025-09-05 20:09:05 -0700

    Update javascript_testing.js

--- javascript_testing.js
+++ javascript_testing.js
@@ -1,5 +1,3 @@
-yyyy
-
 // 3D Javacript ETA 6497 Clock using three.js
 // MIT License. - Work In Progress
 // Jeff Miller 2025. 9/4/25
@@ -992,4 +990,5 @@ function animate() { //
 
 // Start the animation
 animate(); //
+
 
